38b670708fde6cfc8f9d22ab78a5a95c93c54f46
LnwDark <04061surichai@gmail.com>
2018-10-15 23:27:49 +0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;port=5432;dbname=marathon_db',
-    'username' => 'postgres',
-    'password' => 'postgres',
+    'dsn' => 'pgsql:host=localhost;port=5432;dbname=xxxxx',
+    'username' => 'xxxx',
+    'password' => 'xxx',
     'charset' => 'utf8',
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
